29b129e4dcd7248610ca290a02b68e0ce10bbede
Asadbek-Kushakov <76949935+Asadbek-Kushakov@users.noreply.github.com>
2021-11-02 00:52:39 +0500

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// module.exports = {
+// 	images: {
+// 		domains: ["links.papareact.com", "fakestoreapi.com"],
+// 	},
+//
+// 	env: {
+// 		stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+// 	},
+// };
+module.exports = {
+	// Webpack 5 is enabled by default
+	// You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
+	webpack5: false,
+}
